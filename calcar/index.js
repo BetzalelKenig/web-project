c2594e238b40ef20b97a0c3cdfe7964c3467e484
BetzalelKenig <59796236+BetzalelKenig@users.noreply.github.com>
2020-05-25 14:00:21 +0300

    ar calc

---
+++ calcar/index.js
@@ -0,0 +1,30 @@
+let input1 = document.getElementById("first-number");
+let input2 = document.getElementById("second-number");
+let results = document.querySelector("ul");
+const operationButtons = document.querySelectorAll("[data-operation]");
+
+const validation = (n) => n.replace(/[^0-9]+/g, "");
+let i1 = parseInt(validation(input1.value));
+let i2 = parseInt(validation(input2.value));
+operationButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    let r = "";
+    console.log(parseFloat(input1.value));
+    switch (button.id) {
+      case "add":
+        r = parseFloat(validation(input1.value)) + parseFloat(validation(input2.value));
+        break;
+      case "sub":
+        r = validation(input1.value) - validation(input2.value);
+        break;
+      case "mul":
+        r = validation(input1.value) * validation(input2.value);
+        break;
+      case "div":
+        r = validation(input1.value) / validation(input2.value);
+        break;
+    }
+
+    results.innerHTML += `<li>${r}</li>`;
+  });
+});
